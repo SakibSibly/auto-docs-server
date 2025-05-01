e8660b0dbb694eeeef5ce13886e74286a60c161a
SakibSibly <sibly@duck.com>
2025-05-01 10:32:13 +0600

    JWT authentication URL implemented

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='get-token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
     path('v1/info/', views.V1ApiGreet.as_view(), name='note-list-create'),
 ]
