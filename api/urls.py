7005a002ce8846a6575640c40f212163b8186015
SakibSibly <sibly@duck.com>
2025-05-01 12:42:58 +0600

    Registration URL and logic implemented

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='get-token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
+    path('register/', views.CustomUserCreate.as_view(), name='register'),
     path('v1/info/', views.V1ApiGreet.as_view(), name='note-list-create'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from . import serializers
+from . import models
+
+class CustomUserCreate(APIView):
+    def post(self, request):
+        serializer = serializers.CustomUserSerializer(data=request.data)
+        
+        if models.CustomUser.objects.filter(email=request.data.get('email')):
+            return Response({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if models.CustomUser.objects.filter(username=request.data.get('username')):
+            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'user': serializer.data}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class V1ApiGreet(APIView):
     def get(self, request):
