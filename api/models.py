12fc910dee6b03e6265f688574c20fb8eb8f5b9c
SakibSibly <sibly@duck.com>
2025-06-12 15:39:22 +0600

    Update "RequestStatus data model added"

--- api/models.py
+++ api/models.py
@@ -9,6 +9,14 @@ class Role(models.Model):
         return self.name
 
 
+class RequestStatus(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Document(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
