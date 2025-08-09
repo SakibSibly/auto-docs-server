dd10f5f3055231b2ce4f29ffb2cbfaea2dfb1f2b
SakibSibly <sibly@duck.com>
2025-08-09 20:39:17 +0600

    Add "University data model added and made foreignkey in the Faculty data model"

--- api/admin.py
+++ api/admin.py
@@ -5,6 +5,7 @@ from . import models
 admin.site.register(models.CustomUser)
 admin.site.register(models.OTP)
 admin.site.register(models.Role)
+admin.site.register(models.University)
 admin.site.register(models.Faculty)
 admin.site.register(models.Department)
 admin.site.register(models.Course)
